05147bc511d7cb6956216bde9b9cd584ea7616f2
Stella Ling <stellalph2@gmail.com>
2022-05-21 22:30:48 +0930

    add gitinore, package json and server.js file

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+// Set up express server & tell node we are creating an express server
+
+const express = require("express");
+const app = express ();
+
+// Sets the port for listener
+
