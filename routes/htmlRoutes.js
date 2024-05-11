99128c69a3ff60d043782516451407a12f20110b
YUVARAJ787 <yuvarajv787@gmail.com>
2024-05-11 08:13:32 +0530

    fixex some bugs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,7 @@
-// DEPENDENCIES
-// Path package to get the correct file path for html
 const path = require("path");
-// Routing
 const express = require("express");
 const htmlRouter = express.Router();
 
-// HTML GET Requests - handles when users "visit" a page - user is shown an HTML page of content
 
 htmlRouter.get("/index", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
@@ -19,12 +15,8 @@ htmlRouter.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-// If no matching route is found default to index
 htmlRouter.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = htmlRouter;
-
-// References
-// https://expressjs.com/en/guide/routing.html
--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-// Dependencies
-// Set up express server & tell node we are creating an express server
-
 const express = require("express");
 const app = express ();
 
-// Sets the port for listener
-const PORT = process.env.PORT || 4000;
+const PORT = 3000;
 
-// Routers
 const apiRouter = require("./routes/apiRoutes");
 const htmlRouter = require("./routes/htmlRoutes");
 
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Middleware 
 app.use(express.static("public"));
-app.use(express.static("db"));
-// order matters here!
+app.use(express.static("datas"));
 app.use(apiRouter);
 app.use(htmlRouter);
 
-// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
-//https://medium.com/@ralph1786/how-to-setup-an-express-server-5fd9cd9ae073
-// Reference on installing the package called as development dependency, meaning that if 
-// our application ever goes into production, this package will not be included.
-// command os npm i -D nodemon - purpose of this package is to watch for any changes in our files
-// and restart the server instead of us having to do that manually ourselves.
